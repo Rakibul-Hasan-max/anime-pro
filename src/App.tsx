eb5610313c097530326c30de52fe55a206d55b94
Rakibul Hasan <rakibulhasan4041@gmail.com>
2026-08-31 22:41:32 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { KageLandingPage } from "./shaders/landing-pages/LandingPages";
+import "./shaders/threeui.css";
+import "./App.css";
+
+export function Scene() {
+  return (
+    <div className="shader-frame">
+      <KageLandingPage
+        headingFont="onest"
+        bodyFont="onest"
+        headingWeight="400"
+        bodyWeight="300"
+        primaryColor="#e0231c"
+        headingSize={46}
+        bodySize={17}
+        headingLetterSpacing={-0.012}
+      />
+    </div>
+  );
+}
+
+export default function App() {
+  return <Scene />;
+}
